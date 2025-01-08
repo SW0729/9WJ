f912a06a647b0fbe34f838dc9e9cda8c3365afab
suk_hoon <jjj11177742@gmail.com>
2025-01-08 10:18:08 +0900

    수정

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,11 +2,15 @@ import warnings  # warnings 모듈 import
 
 # DeprecationWarning 무시
 warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
 from django.shortcuts import render
 from rest_framework.views import APIView  # DRF에서 제공하는 기본 APIView 클래스
 from rest_framework.response import Response  # 클라이언트에게 응답을 보내는 클래스
 from rest_framework import status  # HTTP 상태 코드를 관리하는 클래스
 from .link import chatbot_response  # link.py에서 AI 응답을 생성하는 함수를 가져옴
+from .calorie_helper import calculate_calories  # 칼로리 계산 함수 호출
+from .KJK_data_collect import collect_recipe_data  # 데이터 수집 함수 호출
 
 class ChatBotAPIView(APIView):
     """
