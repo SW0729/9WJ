23cf69ed45a3256d547fb43fbbe5af925f28ad88
SW0729 <savermarine@gmail.com>
2025-01-30 10:59:28 +0900

    Delete chatbot directory

--- chatbot/file.py
+++
@@ -1,8 +0,0 @@
-from dotenv import dotenv_values
-
-# 컴퓨터의 환경변수보다 .env 우선순위를 높게 설정
-config = dotenv_values(".env")
-# 설정한 변수로부터 값 추출
-OPENAI_API_KEY = config.get('OPENAI_API_KEY2')
-# 최신화된 key가 나옴 (.env 파일 값을 변경해보면서 확인 필수!)
-print(OPENAI_API_KEY)
--- chatbot/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Recipe(models.Model):
-    country_food = models.CharField(max_length=20)  # 요리 종류 (한식, 양식 등)
-    content = models.TextField()  # 레시피 내용
-
-    def __str__(self):
-        return f"{self.food_type}: {self.content[:20]}"
